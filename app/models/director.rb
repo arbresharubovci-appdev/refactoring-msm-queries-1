be5714f6438bf40fd0ea104e6d5afa48f2e6e818
Arbreshë Rubovci <arbresharubovci@gmail.com>
2023-03-10 21:31:29 +0000

    progres

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,4 +11,23 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
+  def filmography
+    my_id = self.id
+    matching_movies = Movie.where({ :director_id => self.id })
+    return matching_movies
+  end
+
+  # three 1=N associations:
+  # - Director => Movie
+  # - Movie => Character
+  # - Actor => Character
+
+  # try define:
+  # - Director#filmography
+  # - Movie#director
+  # - Movie#characters
+  # - Character#movie
+  # - Actor#Cgaracters
+  # - Character#actor
+
 end
